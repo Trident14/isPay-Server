23047464b6c5b40f3d245a3ef116edbed969ec10
Trident14 <sekarinesh14@gmail.com>
2025-02-01 20:17:05 +0530

    fix entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,14 @@
-import express from 'express';
+import cors from 'cors'
 import dotenv from 'dotenv';
+import app from './server.js'
 
+app.use(cors())
 dotenv.config();
-
-const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World!')
+  ;
 });
 
 app.listen(port, () => {
